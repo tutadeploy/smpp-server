80a0ba93f94716ed05275ca8e5143161f7c22e0f
josiah <tutadeploy@tutamail.com>
2025-05-05 17:35:34 +0800

    feat: Update kafka-init-topics-prod.sh

--- kafka-init-topics-prod.sh
+++ kafka-init-topics-prod.sh
@@ -8,5 +8,5 @@ TOPICS=(
 )
 for topic in "${TOPICS[@]}"; do
     docker compose -f docker-compose.prod.yml exec kafka \
-        kafka-topics.sh --create --if-not-exists --topic "$topic" --bootstrap-server $BROKER --partitions 3 --replication-factor 1
+        kafka-topics --create --if-not-exists --topic "$topic" --bootstrap-server $BROKER --partitions 8 --replication-factor 1
 done
